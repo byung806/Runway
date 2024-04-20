e884578dd921f65480af1e30ae8e51607116e427
Bryan Yung <byung806@gmail.com>
2024-04-20 01:56:34 -0400

    Refactor CSS to remove hardcoded main button height

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Pressable, Animated } from 'react-native';
+import { View, Text, Pressable, Animated } from 'react-native';
 
 import { Colors, Styles } from '../styles';
 
-export const BUTTON_HEIGHT = 45;
+// export const BUTTON_HEIGHT = 45;
 export const ANIM_CONFIG = { duration: 30, useNativeDriver: true };
 
 interface MainButtonProps {
@@ -38,81 +38,52 @@ export default function MainButton({ label, callback, disabled = false, filled =
 
     if (disabled) {
         return (
-            <View style={styles.mainDisabledButton}>
-                <Text style={styles.btnDisabledLabel}>{label}</Text>
+            <View style={{
+                borderRadius: 14,
+                backgroundColor: Colors.light.gray,
+                ...Styles.centeredContainer,
+                padding: 10,
+            }}>
+                <Text style={{
+                    ...Styles.btnLabel,
+                    color: '#aaaaaa',
+                }}>{label}</Text>
             </View>
         );
     } else {
         return (
             <Pressable onPressIn={onPressIn} onPressOut={onPressOut}>
-                <View style={styles.outsideView}>
-                    <Animated.View style={[filled ? styles.filledMainButton : styles.unfilledMainButton, { transform: [{ translateY }] }]}>
-                        <Text style={filled ? styles.filledLabel : styles.unfilledLabel}>{label}</Text>
-                    </Animated.View>
-                    <View style={filled ? styles.filledBackground : styles.unfilledBackground} />
+                <Animated.View style={{
+                    borderRadius: 14,
+                    marginBottom: 2,
+                    backgroundColor: filled ? Colors.light.accent : Colors.light.white,
+                    borderColor: Colors.light.gray,
+                    borderWidth: filled ? 0 : 2,
+                    padding: 10,
+                    ...Styles.centeredContainer,
+                    transform: [{ translateY }]
+                }}>
+                    <Text style={{
+                        ...Styles.btnLabel,
+                        color: filled ? Colors.light.white : Colors.light.accent,
+                    }}>{label}</Text>
+                </Animated.View>
+                <View style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    zIndex: -1,
+                }}>
+                    <View style={{
+                        flex: 1,
+                        backgroundColor: filled ? Colors.light.accentDarker : Colors.light.gray,
+                        marginTop: 2,
+                        borderRadius: 14,
+                        zIndex: -1,
+                        bottom: 0,
+                    }} />
                 </View>
             </Pressable>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    mainDisabledButton: {
-        height: BUTTON_HEIGHT,
-        borderRadius: 14,
-        backgroundColor: Colors.light.gray,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    btnDisabledLabel: {
-        ...Styles.btnLabel,
-        color: '#aaaaaa',
-    },
-    outsideView: {
-        height: BUTTON_HEIGHT,
-    },
-    filledBackground: {
-        backgroundColor: Colors.light.accentDarker,
-        width: '100%',
-        height: BUTTON_HEIGHT - 2,
-        borderRadius: 14,
-        position: 'absolute',
-        zIndex: -1,
-        bottom: 0,
-    },
-    unfilledBackground: {
-        backgroundColor: Colors.light.gray,
-        width: '100%',
-        height: BUTTON_HEIGHT - 2,
-        borderRadius: 14,
-        position: 'absolute',
-        zIndex: -1,
-        bottom: 0,
-    },
-    filledMainButton: {
-        height: BUTTON_HEIGHT - 2,
-        borderRadius: 14,
-        backgroundColor: Colors.light.accent,
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    unfilledMainButton: {
-        height: BUTTON_HEIGHT - 2,
-        borderRadius: 14,
-        borderWidth: 2,
-        borderColor: Colors.light.gray,
-        backgroundColor: Colors.light.white,
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    filledLabel: {
-        ...Styles.btnLabel,
-        color: Colors.light.white,
-    },
-    unfilledLabel: {
-        ...Styles.btnLabel,
-        color: Colors.light.accent,
-    }
-});
