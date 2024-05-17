5b4f0c71c1b2b19436e5de8af964c00921892293
Bryan Yung <byung806@gmail.com>
2024-05-17 18:08:06 -0400

    Implement plane sprite in React Three Fiber

--- app/(screens)/home.tsx
+++ app/(screens)/home.tsx
@@ -1,21 +1,32 @@
-import { MainButton } from "@/components";
-import React from "react";
+import { MainButton } from "@/components/screens";
+import React, { Suspense } from "react";
 import { View, Text } from "react-native";
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { Debug, Styles } from "@/styles";
 
-import Plane from "@/components/Plane";
+import Plane from "@/components/game/Plane";
 
-
+import { Canvas } from "@react-three/fiber";
+import useControls from "r3f-native-orbitcontrols";
 
 
 export default function HomeScreen({ navigation }: { navigation: NativeStackNavigationProp<any, any> }) {
+    const [OrbitControls, setOrbitControls] = useControls();
+
     return (
-        <View style={{
-            ...Styles.centeredContainer,
-            padding: 50
-        }}>
-            <Plane size={144} />
-        </View>
+        <Canvas
+        // orthographic
+        // camera={{ position: [1, 1, 1], scale: [0.01, 0.01, 0.01] }}
+        camera={{ position: [1.5, 1.5, 1.5] }}
+        >
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[-1, 2, 2]} intensity={4} />
+            <pointLight position={[1, 0, 0]} intensity={10} />
+            <Suspense fallback={null}>
+                <Plane />
+                {/* <TwistedBox /> */}
+            </Suspense>
+            <OrbitControls />
+        </Canvas>
     );
 };
