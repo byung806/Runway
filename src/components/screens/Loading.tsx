d9d3c610d8b76fb7093ff7c93b3bb112f202a72b
Bryan Yung <byung806@gmail.com>
2024-05-24 00:03:12 -0400

    Create loading screen

---
+++ src/components/screens/Loading.tsx
@@ -0,0 +1,44 @@
+import { FontAwesome as Icon } from '@expo/vector-icons';
+import { Colors, Debug, Styles } from "@/styles";
+import { Animated, Easing, Image, Text, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+
+export default function Loading({ id = 0, size = 144 }: { id?: number, size?: number }) {
+    const animated = new Animated.Value(0);
+    const duration = 4000;
+
+    useEffect(() => {
+        Animated.loop(
+            Animated.timing(
+                animated,
+                {
+                    toValue: 1,
+                    duration: duration,
+                    easing: Easing.linear,
+                    useNativeDriver: true
+                }
+            )
+        ).start();
+    }, []);
+
+    const spin = animated.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg']
+    })
+
+    return (
+        // <Icon name="paper-plane" size={size} color={Colors.light.accent} />
+        <View style={{...Styles.flex, ...Styles.centeredContainer}}>
+            <Animated.View
+                style={{
+                    transform: [{ rotate: spin }]
+                }}
+            >
+                <Image
+                    source={require('@/assets/planes/plane0.png')}
+                    style={{ width: size, height: size }}
+                />
+            </Animated.View>
+        </View>
+    )
+}
