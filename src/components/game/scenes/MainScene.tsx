bcbd0336ec048cc86985959d47e1e0c8fc6a1490
Bryan Yung <byung806@gmail.com>
2024-06-14 01:12:46 -0400

    Link inside Canvas functions with outside UI (useImperativeHandle), add plane animations

--- src/components/game/scenes/MainScene.tsx
+++ src/components/game/scenes/MainScene.tsx
@@ -1,14 +1,15 @@
 import { Canvas, useFrame, useThree } from "@react-three/fiber/native";
 import useControls from "r3f-native-orbitcontrols";
 import React, { Suspense, useRef } from "react";
-import Plane from "../Plane";
+import { Plane, PlaneRef } from "../Plane";
 import Particles from "../ParticleSphere";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 import { Styles } from "@/styles";
 import { Sky } from "@react-three/drei/native";
 import { Vector3, Group } from "three";
 import { useTheme } from "@react-navigation/native";
 import Ground from "../Ground";
+import Ocean from "../Ocean";
 
 
 function Rig({ children }: { children: React.ReactNode }) {
@@ -33,25 +34,45 @@ function Rig({ children }: { children: React.ReactNode }) {
 
 export default function MainScene({ referenceSphere = false, props }: { referenceSphere?: boolean, props?: any }) {
     const { colors } = useTheme();
+    // TODO: landing/takeoff
+    // TODO: different planes
+    // TODO: different destinations / daily location
+    // TODO: tilt plane towards camera
     // TODO: add a way to change sky color
+    // TODO: ticking clock ui for next destination
+    const plane = useRef<PlaneRef>();
+    const ground = useRef();
 
     const [OrbitControls, events] = useControls();
 
+    function newDay(islandFile?: string) {
+        if (!plane.current || !ground.current) return;
+        plane.current.devToggle();
+    }
+
     return (
         <View style={Styles.flex} {...events} {...props}>
+            <Button title="new day (dev)" onPress={() => {newDay('file')}} />
             <Canvas
+                shadows
                 orthographic
-                camera={{ position: [0, 0, 1], zoom: 100 }}
+                camera={{ position: [0, 10, 0], zoom: 100 }}
                 onCreated={(state) => { const _gl = state.gl.getContext(); const pixelStorei = _gl.pixelStorei.bind(_gl); _gl.pixelStorei = function(...args) { const [parameter] = args; switch(parameter) { case _gl.UNPACK_FLIP_Y_WEBGL: return pixelStorei(...args) } } }}
             >
-                {/* <OrbitControls
+            {/* <Canvas
+                shadows
+                camera={{ position: [0, 10, 0], zoom: 3, rotation: [0, Math.PI/3, 0] }}
+                onCreated={(state) => { const _gl = state.gl.getContext(); const pixelStorei = _gl.pixelStorei.bind(_gl); _gl.pixelStorei = function(...args) { const [parameter] = args; switch(parameter) { case _gl.UNPACK_FLIP_Y_WEBGL: return pixelStorei(...args) } } }}
+            > */}
+                <OrbitControls
                     enablePan={false}
-                /> */}
-                <ambientLight intensity={2} />
+                />
+                <ambientLight intensity={1} />
+                <directionalLight castShadow intensity={2} />
                 <Suspense fallback={null}>
-                    <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} {...props} />
                     <Rig>
-                        <Plane level={0} />
+                        {/* @ts-expect-error */}
+                        <Plane ref={plane} planeType={0} />
                         <Ground />
                         {referenceSphere && <Particles />}
                     </Rig>
