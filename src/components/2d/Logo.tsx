e328a63c89911639ff0cb1d5504f05f97abd2cbd
Bryan Yung <byung806@gmail.com>
2024-06-18 01:01:32 -0400

    Tweak logo size

--- src/components/2d/Logo.tsx
+++ src/components/2d/Logo.tsx
@@ -1,7 +1,7 @@
 import { FontAwesome as Icon } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
 
-export default function Logo({size=144}: {size?: number}) {
+export default function Logo({size=80}: {size?: number}) {
     const { colors } = useTheme();
 
     return (
