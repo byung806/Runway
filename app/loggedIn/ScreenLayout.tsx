630cc39ae08d2314b57b163e53ffee7eb22aa7ea
Bryan Yung <byung806@gmail.com>
2024-06-20 15:54:49 -0400

    Animate bottom tabs

--- app/loggedIn/ScreenLayout.tsx
+++ app/loggedIn/ScreenLayout.tsx
@@ -1,37 +1,46 @@
 import Book from '@/assets/svg/book.svg';
 import Home from '@/assets/svg/home.svg';
 import Trophy from '@/assets/svg/trophy.svg';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useTheme } from '@react-navigation/native';
 
 import ContentScreen from './ContentScreen';
 import HomeScreen from './HomeScreen';
 import LeaderboardScreen from './LeaderboardScreen';
+import { Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialTopTabNavigator();
 
 export default function ScreenLayout() {
     const { colors } = useTheme();
 
     return (
-        <Tab.Navigator
-            initialRouteName="Home"
-            screenOptions={({ route }) => ({
-                tabBarShowLabel: false,
-                tabBarIcon: ({ focused }) => {
-                    if (route.name === 'Content') {
-                        return <Book width={30} height={30} fill={focused ? colors.primary : colors.text} />;
-                    } else if (route.name === 'Home') {
-                        return <Home width={30} height={30} fill={focused ? colors.primary : colors.text} />;
-                    } else if (route.name === 'Leaderboard') {
-                        return <Trophy width={30} height={30} fill={focused ? colors.primary : colors.text} />;
-                    }
-                },
-            })}
-        >
-            <Tab.Screen name="Content" component={ContentScreen} options={{ headerShown: false }} />
-            <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-            <Tab.Screen name="Leaderboard" component={LeaderboardScreen} options={{ headerShown: false }} />
-        </Tab.Navigator>
+        <SafeAreaView style={{ flex: 1 }} edges={['bottom']}>
+            <Tab.Navigator
+                initialRouteName="Home"
+                screenOptions={({ route }) => ({
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused }) => {
+                        if (route.name === 'Content') {
+                            return <Book width={30} height={30} fill={focused ? colors.primary : colors.text} />;
+                        } else if (route.name === 'Home') {
+                            return <Home width={30} height={30} fill={focused ? colors.primary : colors.text} />;
+                        } else if (route.name === 'Leaderboard') {
+                            return <Trophy width={30} height={30} fill={focused ? colors.primary : colors.text} />;
+                        }
+                    },
+                    tabBarPressColor: 'transparent',
+                    tabBarBounces: true,
+                    swipeEnabled: false,
+                })}
+                tabBarPosition="bottom"
+                initialLayout={{ width: Dimensions.get('window').width }}
+            >
+                <Tab.Screen name="Content" component={ContentScreen} />
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Leaderboard" component={LeaderboardScreen} />
+            </Tab.Navigator>
+        </SafeAreaView>
     );
 }
