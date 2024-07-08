289d9c804cdb0b7dc59ab5cdba416991a1fbff33
Bryan Yung <byung806@gmail.com>
2024-07-08 15:33:34 -0400

    Edit navbar

--- src/components/2d/AnimatedIcon.tsx
+++ src/components/2d/AnimatedIcon.tsx
@@ -1,62 +1,54 @@
-import { Styles } from "@/styles";
-import React, { useContext, useEffect } from "react";
-import Animated, { Easing, interpolateColor, useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming } from "react-native-reanimated";
+import { animated, config, useSpring } from "@react-spring/native";
+import React, { forwardRef, useContext, useImperativeHandle, useState } from "react";
 import { ThemeContext } from "./ThemeProvider";
 
 import Book from '@/assets/svg/book.svg';
 import Home from '@/assets/svg/home.svg';
 import Trophy from '@/assets/svg/trophy.svg';
+import { useNavigation } from "@react-navigation/native";
+import { View } from "react-native";
 
-export default function AnimatedIcon({ focused, route, width, height }: { focused: boolean, route: any, width: number, height: number }) {
+const AnimatedView = animated(View);
+
+export type AnimatedIconRef = {
+    setActive: (active: boolean) => void;
+}
+
+const AnimatedIcon = forwardRef(({ focused, route, width, height }: { focused: boolean, route: any, width: number, height: number }, ref) => {
     const theme = useContext(ThemeContext);
+    const navigation = useNavigation();
 
-    const scale = useSharedValue<number>(1);
-    const opacity = useSharedValue<number>(0);
+    const [active, setActive] = useState(false);
+    const springs = useSpring({
+        scale: active ? 1.2 : 1,
+        config: config.wobbly,
+    })
 
-    const animatedScale = useAnimatedStyle(() => ({
-        transform: [{ scale: scale.value }],
+    const opacity = useSpring({
+        backgroundColor: focused ? theme.accent : '#00000000',
+    })
+
+    useImperativeHandle(ref, () => ({
+        setActive,
     }));
 
-    const animatedOpacity = useAnimatedStyle(() => {
-        return {
-            backgroundColor: interpolateColor(
-                opacity.value,
-                [0, 1],
-                ['transparent', theme.accent]
-            ),
-        };
-    });
-
-    useEffect(() => {
-        if (focused) {
-            opacity.value = withTiming(1);
-            scale.value = withSequence(
-                withTiming(1.2, {
-                    duration: 30,
-                    easing: Easing.inOut(Easing.ease),
-                }),
-                withTiming(1)
-            );
-        } else {
-            console.log('not focused');
-            opacity.value = withTiming(0);
-            scale.value = withTiming(1);
-        }
-    }, [focused]);
-
     return (
-        <Animated.View style={animatedScale}>
-            <Animated.View style={[{
+        <AnimatedView style={{ transform: [{ scale: springs.scale }] }}>
+            <AnimatedView style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
                 width: width + 20,
                 height: height + 20,
                 borderRadius: 20,
-                // elevation: 1,
-                ...Styles.centeringContainer,
-            }, animatedOpacity]}>
-                {route.name === 'content' && <Book width={width} height={height} fill={focused ? theme.textInverse : theme.text} style={{elevation: 10}} />}
+                backgroundColor: opacity.backgroundColor,
+            }}>
+                {route.name === 'content' && <Book width={width} height={height} fill={focused ? theme.textInverse : theme.text} style={{ elevation: 10 }} />}
                 {route.name === 'home' && <Home width={width} height={height} fill={focused ? theme.textInverse : theme.text} />}
                 {route.name === 'leaderboard' && <Trophy width={width} height={height} fill={focused ? theme.textInverse : theme.text} />}
-            </Animated.View>
-        </Animated.View>
+            </AnimatedView>
+        </AnimatedView>
     )
-}
+});
+
+export default AnimatedIcon;
