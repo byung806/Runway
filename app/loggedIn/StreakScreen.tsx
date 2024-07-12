7db3f9a53bc3600bae8d75e5f8becac805c43693
Bryan Yung <byung806@gmail.com>
2024-07-12 14:24:37 -0400

    Reposition StreakScreen ui

--- app/loggedIn/StreakScreen.tsx
+++ app/loggedIn/StreakScreen.tsx
@@ -1,7 +1,7 @@
 import { Styles } from "@/styles";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useContext, useEffect, useState } from "react";
-import { View } from "react-native";
+import { Easing, View } from "react-native";
 import Animated, { useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming } from "react-native-reanimated";
 import { Button, Text, ThemeContext } from "~/2d";
 import * as Haptics from "expo-haptics";
@@ -11,61 +11,65 @@ import LottieView from 'lottie-react-native';
 
 // i put this screen as a tab in the app just so you can easily access it
 // but in the final app this screen will be hidden and pop up when the streak increases
-export default function StreakScreen() {
+export default function StreakScreen({ startingStreak }: { startingStreak: number }) {
     const theme = useContext(ThemeContext);
 
-    const [streak, setStreak] = useState(0);
-
-    // TODO: animate the streak icon and the number transition (u can choose how to animate it)
-    // this scale variable is just a variable representing a number that can be animated
-    const scale = useSharedValue<number>(2);
-
-    // this animatedScale variable is a style that can be applied to a component
-    const animatedScale = useAnimatedStyle(() => ({
-        transform: [{ scale: scale.value }],
-    }));
+    const [streak, setStreak] = useState(startingStreak);
 
     // this useEffect is called when the streak variable changes
     // so when the streak changes, we can animate the icon or something
-    useEffect(() => {
-        console.log('from StreakScreen.tsx:  useEffect');
+    // useEffect(() => {
+    //     console.log('from StreakScreen.tsx:  useEffect');
 
-        // i just made a really dumb animation here but i want u to make it better
-        // you can learn how to use different reanimated functions here: https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/your-first-animation
-        scale.value = withSequence(
-            withTiming(1.3, {duration: 100}),
-            withTiming(1, {duration: 100}),
-        )
+    //     // i just made a really dumb animation here but i want u to make it better
+    //     // you can learn how to use different reanimated functions here: https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/your-first-animation
+    //     scale.value = withSequence(
+    //         withTiming(1.3, { duration: 100 }),
+    //         withTiming(1, { duration: 100 }),
+    //     )
 
-    }, [streak]);
+    // }, [streak]);
 
     return (
         <View style={{
             flex: 1
         }}>
-            <LottieView
+            <View style={{
+                flex: 1,
+                ...Styles.centeringContainer,
+            }}>
+                {/* all of this is just random stuff i threw together to give an example */}
+                {/* this is duolingo's animation for inspiration https://youtu.be/XL5ALfc2R-A?t=153 */}
+                <LottieView
+                    style={{
+                        width: 500,
+                        height: 500,
+                        position: 'absolute',
+                        top: 0,
+                    }}
                     source={require('@/assets/pixelFire.json')}
                     autoPlay
                     loop
-                    style={{ width: 200, height: 200 }}/>
-            <Animated.View style={[{
-                flex: 1,
-                ...Styles.centeringContainer,
-            }, animatedScale]}>
-                {/* all of this is just random stuff i threw together to give an example */}
-                {/* this is duolingo's animation for inspiration https://youtu.be/XL5ALfc2R-A?t=153 */}
-                <Button title="reset to 0 (dev)" onPress={() => setStreak(0)} />
-                <Button title="increment (dev)" onPress={() => setStreak(streak + 100)} />
-                <MaterialCommunityIcons name="fire" size={100} color={theme.accent} />
-                <AnimatedNumbers animateToNumber={streak} fontStyle={{color: theme.text, fontSize:100, textAlign: 'center', fontFamily: 'Silkscreen_400Regular'}}/>
-
-                <Text style={{
+                />
+                <AnimatedNumbers
+                    animateToNumber={streak}
+                    fontStyle={{ color: theme.textInverse, fontSize: 100, textAlign: 'center', fontFamily: 'Silkscreen_400Regular' }}
+                    easing={Easing.out(Easing.cubic)}
+                />
+                {/* <Text style={{
                     color: theme.text,
                     fontSize: 20,
                     textAlign: 'center',
                     marginTop: 20,
-                }}>Days in a row!</Text>
-            </Animated.View>
+                }}>Days in a row!</Text> */}
+
+                <Button title="reset to 0 (dev)" onPress={() => setStreak(0)} style={{
+                    zIndex: 1000,
+                }} />
+                <Button title="increment (dev)" onPress={() => setStreak(streak + 1)} style={{
+                    zIndex: 1000,
+                }} />
+            </View>
 
         </View>
     );
