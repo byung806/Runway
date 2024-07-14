d7148afb7b74db20237f90694795583d1e7305bd
Bryan Yung <byung806@gmail.com>
2024-07-13 22:23:41 -0400

    Implement streak screen

--- app/loggedIn/StreakScreen.tsx
+++ app/loggedIn/StreakScreen.tsx
@@ -1,74 +1,112 @@
 import { Styles } from "@/styles";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { useContext, useEffect, useState } from "react";
-import { Easing, View } from "react-native";
-import Animated, { useAnimatedStyle, useSharedValue, withSequence, withSpring, withTiming } from "react-native-reanimated";
-import { Button, Text, ThemeContext } from "~/2d";
-import * as Haptics from "expo-haptics";
-import AnimatedNumbers from 'react-native-animated-numbers';
+import FadeIn, { FadeInRef } from "@/utils/FadeIn";
+import { delay } from "@/utils/utils";
+import { useIsFocused } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { animated } from "@react-spring/native";
 import LottieView from 'lottie-react-native';
+import { useContext, useEffect, useRef, useState } from "react";
+import { Easing, View } from "react-native";
+import AnimatedNumbers from 'react-native-animated-numbers';
+import { Button, Text, ThemeContext } from "~/2d";
 
 
-// i put this screen as a tab in the app just so you can easily access it
-// but in the final app this screen will be hidden and pop up when the streak increases
-export default function StreakScreen({ startingStreak }: { startingStreak: number }) {
+const AnimatedButton = animated(Button);
+
+export default function StreakScreen({ navigation, ...props }: { navigation: StackNavigationProp<any, any> } & any) {
     const theme = useContext(ThemeContext);
+    const focused = useIsFocused();
 
-    const [streak, setStreak] = useState(startingStreak);
+    const [streak, setStreak] = useState(props.startingStreak || 0);
+    const [buttonClickable, setButtonClickable] = useState(false);
 
-    // this useEffect is called when the streak variable changes
-    // so when the streak changes, we can animate the icon or something
-    // useEffect(() => {
-    //     console.log('from StreakScreen.tsx:  useEffect');
+    const fadeInRef = useRef<FadeInRef>(null);
 
-    //     // i just made a really dumb animation here but i want u to make it better
-    //     // you can learn how to use different reanimated functions here: https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/your-first-animation
-    //     scale.value = withSequence(
-    //         withTiming(1.3, { duration: 100 }),
-    //         withTiming(1, { duration: 100 }),
-    //     )
+    const numberAnimationDuration = 1400;
+    const fireAnimationRef = useRef<LottieView>(null);
 
-    // }, [streak]);
+    useEffect(() => {
+        incrementStreak();
+    }, []);
+
+    useEffect(() => {
+        if (buttonClickable) fadeInRef.current?.start();
+    }, [buttonClickable]);
+
+    useEffect(() => {
+        if (!focused) {
+            fireAnimationRef.current?.reset();
+        }
+    }, [focused]);
+
+    async function incrementStreak() {
+        await delay(700);
+        setStreak(streak + 1);
+        await delay(numberAnimationDuration + 100);
+        fireAnimationRef.current?.play();
+        await delay(300);
+        setButtonClickable(true);
+    }
 
     return (
         <View style={{
-            flex: 1
+            flex: 1,
+            backgroundColor: theme.background,
         }}>
             <View style={{
                 flex: 1,
-                ...Styles.centeringContainer,
+                // ...Styles.centeringContainer,
+                // ...Styles.debugBorder,
             }}>
                 {/* all of this is just random stuff i threw together to give an example */}
                 {/* this is duolingo's animation for inspiration https://youtu.be/XL5ALfc2R-A?t=153 */}
-                <LottieView
-                    style={{
-                        width: 500,
-                        height: 500,
-                        position: 'absolute',
-                        top: 0,
-                    }}
-                    source={require('@/assets/pixelFire.json')}
-                    autoPlay
-                    loop
-                />
-                <AnimatedNumbers
-                    animateToNumber={streak}
-                    fontStyle={{ color: theme.textInverse, fontSize: 100, textAlign: 'center', fontFamily: 'Silkscreen_400Regular' }}
-                    easing={Easing.out(Easing.cubic)}
-                />
-                {/* <Text style={{
-                    color: theme.text,
-                    fontSize: 20,
-                    textAlign: 'center',
-                    marginTop: 20,
-                }}>Days in a row!</Text> */}
+                <View style={{
+                    flex: 1,
+                    ...Styles.centeringContainer,
+                }}>
+                    <LottieView
+                        ref={fireAnimationRef}
+                        style={{
+                            width: 500,
+                            height: 500,
+                            position: 'absolute',
+                            top: 20,
+                        }}
+                        source={require('@/assets/pixelFire.json')}
+                        loop
+                    />
+                    <AnimatedNumbers
+                        animateToNumber={streak}
+                        animationDuration={numberAnimationDuration}
+                        fontStyle={{ color: theme.text, fontSize: 100, textAlign: 'center', fontFamily: 'Silkscreen_400Regular' }}
+                        easing={Easing.out(Easing.cubic)}
+                    />
+                    <Text style={{
+                        color: theme.text,
+                        fontSize: 20,
+                        textAlign: 'center',
+                        marginBottom: 20,
+                    }}>day streak</Text>
+                </View>
 
-                <Button title="reset to 0 (dev)" onPress={() => setStreak(0)} style={{
-                    zIndex: 1000,
-                }} />
-                <Button title="increment (dev)" onPress={() => setStreak(streak + 1)} style={{
-                    zIndex: 1000,
-                }} />
+                <View style={{
+                    width: '90%',
+                    position: 'absolute',
+                    bottom: 0,
+                    left: '5%',
+                }}>
+                    <FadeIn ref={fadeInRef}>
+                        <AnimatedButton
+                            style={{
+                                marginBottom: 20,
+                            }}
+                            disabled={!buttonClickable}
+                            title={'Continue'}
+                            filled={false}
+                            onPress={() => navigation.navigate('app')}
+                        />
+                    </FadeIn>
+                </View>
             </View>
 
         </View>
---
+++ src/utils/utils.ts
@@ -0,0 +1,4 @@
+export const delay = async (ms: any) => {
+    return new Promise((resolve) =>
+        setTimeout(resolve, ms));
+};
