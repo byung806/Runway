a5dcf14d968694f4f3800084986af940ea28ec3d
Bryan Yung <byung806@gmail.com>
2024-07-15 16:12:12 -0400

    Update build settings for ios

--- src/components/3d/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MainScene } from './scenes/MainScene';
-export { GroundRef, Ground } from './Ground';
-export { PlaneRef, Plane } from './Plane';
-export { default as ParticleSphere } from './ParticleSphere';
