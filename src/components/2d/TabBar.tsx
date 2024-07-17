f655808826eb16807cb13beca8df3a36206a598c
Bryan Yung <byung806@gmail.com>
2024-07-17 14:11:38 -0400

    Fix TabBar & SafeAreaView spacing

--- src/components/2d/TabBar.tsx
+++ src/components/2d/TabBar.tsx
@@ -3,13 +3,14 @@ import * as Haptics from 'expo-haptics';
 import { useContext, useRef } from "react";
 import { Pressable, View } from "react-native";
 import AnimatedIcon, { AnimatedIconRef } from "./AnimatedIcon";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { ThemeContext } from "./ThemeProvider";
 
 export default function TabBar({ state, descriptors, navigation }: { state: any, descriptors: any, navigation: any }) {
     const theme = useContext(ThemeContext);
 
     let padding = 10;
-    
+
     return (
         <View style={{
             position: 'absolute',
@@ -20,62 +21,64 @@ export default function TabBar({ state, descriptors, navigation }: { state: any,
             marginVertical: 10,
             ...Styles.centeringContainer,
         }}>
-            <View style={{
-                flexDirection: 'row',
-                // backgroundColor: theme.accent + '33',
-                borderRadius: 24,
-                // height: 70,
-                padding: padding,
-                // elevation: 4,
-            }}>
-                {state.routes.map((route: any, index: any) => {
-                    const isFocused = state.index === index;
-                    const iconRef = useRef<AnimatedIconRef>(null);
+            <SafeAreaView edges={['bottom']}>
+                <View style={{
+                    flexDirection: 'row',
+                    // backgroundColor: theme.accent + '33',
+                    borderRadius: 24,
+                    // height: 70,
+                    padding: padding,
+                    // elevation: 4,
+                }}>
+                    {state.routes.map((route: any, index: any) => {
+                        const isFocused = state.index === index;
+                        const iconRef = useRef<AnimatedIconRef>(null);
 
-                    const onPress = () => {
-                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+                        const onPress = () => {
+                            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
 
-                        iconRef.current?.onPressIn();
+                            iconRef.current?.onPressIn();
 
-                        const event = navigation.emit({
-                            type: 'tabPress',
-                            target: route.key,
-                        });
+                            const event = navigation.emit({
+                                type: 'tabPress',
+                                target: route.key,
+                            });
 
-                        if (!isFocused && !event.defaultPrevented) {
-                            navigation.navigate(route.name);
+                            if (!isFocused && !event.defaultPrevented) {
+                                navigation.navigate(route.name);
+                            }
+                        };
+
+                        const onPressOut = () => {
+                            iconRef.current?.onPressOut();
                         }
-                    };
 
-                    const onPressOut = () => {
-                        iconRef.current?.onPressOut();
-                    }
+                        const onLongPress = () => {
+                            navigation.emit({
+                                type: 'tabLongPress',
+                                target: route.key,
+                            });
+                        };
 
-                    const onLongPress = () => {
-                        navigation.emit({
-                            type: 'tabLongPress',
-                            target: route.key,
-                        });
-                    };
-
-                    let spaceBetween = 30;
-                    return (
-                        <View style={{
-                            ...Styles.centeringContainer,
-                            marginLeft: index === 0 ? 0 : spaceBetween,
-                        }} key={index}>
-                            <Pressable
-                                onPress={onPress}
-                                onPressOut={onPressOut}
-                                onLongPress={onLongPress}
-                                style={{ alignItems: "center" }}
-                            >
-                                <AnimatedIcon ref={iconRef} focused={isFocused} route={route} width={30} height={30} />
-                            </Pressable>
-                        </View>
-                    );
-                })}
-            </View>
+                        let spaceBetween = 30;
+                        return (
+                            <View style={{
+                                ...Styles.centeringContainer,
+                                marginLeft: index === 0 ? 0 : spaceBetween,
+                            }} key={index}>
+                                <Pressable
+                                    onPress={onPress}
+                                    onPressOut={onPressOut}
+                                    onLongPress={onLongPress}
+                                    style={{ alignItems: "center" }}
+                                >
+                                    <AnimatedIcon ref={iconRef} focused={isFocused} route={route} width={30} height={30} />
+                                </Pressable>
+                            </View>
+                        );
+                    })}
+                </View>
+            </SafeAreaView>
         </View>
     );
 }
