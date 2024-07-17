d86db6fe2f351b61bf035da5acd78d26bab23afb
Bryan Yung <byung806@gmail.com>
2024-07-17 14:10:26 -0400

    Add date utils

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,3 +2,25 @@ export function getTodayDate(): string {
     const today = new Date().toISOString();
     return today.split('T')[0];
 }
+
+export function dateToString(date: Date): string {
+    return date.toISOString().split('T')[0];
+}
+
+export function stringToDate(dateString: string): Date {
+    return new Date(dateString + 'T00:00:00');
+}
+
+export function sameDay(today: Date, date: Date): boolean {
+    return (today.getDate() === date.getDate()
+        && today.getMonth() === date.getMonth()
+        && today.getFullYear() === date.getFullYear())
+}
+
+export function dayIsYesterday(today: Date, date: Date): boolean {
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    return (yesterday.getDate() === date.getDate()
+        && yesterday.getMonth() === date.getMonth()
+        && yesterday.getFullYear() === date.getFullYear())
+}
