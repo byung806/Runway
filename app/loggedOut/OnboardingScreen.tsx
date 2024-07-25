01ef1fbb5a397985c972d7b75c9d9eaec18dce9b
Jacob <jacoblokheenlee9@gmail.com>
2024-07-24 23:23:23 -0400

    changes

--- app/loggedOut/OnboardingScreen.tsx
+++ app/loggedOut/OnboardingScreen.tsx
@@ -1,143 +1,172 @@
-import React, { useContext, useRef, useState } from 'react';
-import { View, KeyboardAvoidingView, Platform, Image, Easing, TextInput } from 'react-native';
-import ViewPager from 'react-native-pager-view';
-import { OnboardingPage, Text, ThemeContext } from '@/components/2d';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { Dimensions, FlatList, Pressable, View } from 'react-native';
+import { DateCard, DateCardRef, ListFooterComponent, ListHeaderComponent, Text, ThemeContext } from '@/components/2d';
 
 import { Styles } from '@/styles';
-import { FontAwesome5, Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { getTodayDate, stringToDate } from '@/utils/date';
+import { Content, ContentColors, useFirebase } from '@/utils/FirebaseProvider';
+import useBounceAnimation from '@/utils/useBounceAnimation';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { StackNavigationProp } from '@react-navigation/stack';
-import AnimatedNumbers from 'react-native-animated-numbers';
-import { delay } from "@/utils/utils";
+import { animated, config } from '@react-spring/native';
+import * as Haptics from 'expo-haptics';
+import Animated, { useSharedValue, withTiming } from 'react-native-reanimated';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import OnboardingCard, { OnboardingCardRef } from '@/components/2d/OnboardingCard';
+import OnboardingHeaderComponent from '@/components/2d/OnboardingHeaderComponent';
 
-export default function OnboardingScreen({ navigation }: { navigation: StackNavigationProp<any, any> }) {
-    //Add number increase animation for next page, new fire animation
-    const pagerRef = useRef<ViewPager>(null);
-    const size = 80;
+
+const ReactSpringAnimatedView = animated(View);
+
+interface OnboardingCardAttributes {
+    ref: OnboardingCardRef | null;
+    colors: ContentColors;
+    index: number;
+}
+
+export default function OnboardingScreen({ navigation, ...props }: { navigation: StackNavigationProp<any, any> } & any) {
     const theme = useContext(ThemeContext);
+    const firebase = useFirebase();
+    const today = getTodayDate();
     const [username, setUsername] = useState('');
-    const [points, setPoints] = useState(0);
 
-    const handlePageChange = (pageNumber: number) => {
-        pagerRef.current?.setPage(pageNumber);
-    };
+    const [focusedIndex, setFocusedIndex] = useState<number | null>(0);
+    const [cards, setCards] = useState<OnboardingCardAttributes[]>([
+        {ref: null, colors: {
+            outerBackgroundColor: "#ffe063",
+            borderColor: "#2cab1c",
+            backgroundColor: "#f4d03e",
+            textColor: "#aa8c16"
+            }, index: 0}, {ref: null, colors: {
+                outerBackgroundColor: "#ffe063",
+                borderColor: "#2cab1c",
+                backgroundColor: "#f4d03e",
+                textColor: "#aa8c16"
+                }, index: 1}, {ref: null, colors: {
+                    outerBackgroundColor: "#ffe063",
+                    borderColor: "#2cab1c",
+                    backgroundColor: "#f4d03e",
+                    textColor: "#aa8c16"
+                    }, index: 2}
+    ]);
+    const todayButtonTransformY = useSharedValue(0);
+    const todayButtonOpacity = useSharedValue(0);
+    const outerBackgroundColor = useSharedValue(theme.background);
+    const goButtonColor = useSharedValue('transparent');
+    const goButtonOpacity = useSharedValue(0);
 
-    async function handlePageChangePoints(pageNumber: number) {
-        pagerRef.current?.setPage(pageNumber);
-        await delay(500);
-        setPoints(99999);
-    };
+    // for GO button
+    const { scale, onPressIn, onPressOut } = useBounceAnimation({
+        scaleTo: 0.9,
+        haptics: Haptics.ImpactFeedbackStyle.Light,
+        config: config.wobbly
+    });
 
-    async function handlePageChangePointsReset(pageNumber: number) {
-        pagerRef.current?.setPage(pageNumber);
-        await delay(500);
-        setPoints(0);
-    };
+    // Change background color when theme changes
+    useEffect(() => {
+        outerBackgroundColor.value = withTiming(theme.background, { duration: 200 });
+    }, [theme]);
+
+    const flatListRef = useRef<FlatList<OnboardingCardAttributes>>(null);
+
+    const paddingAboveHeader = 50;
+    const headerHeight = Dimensions.get("window").height * 0.8;
+
+    const padding = 30;
+    const boxWidth = Dimensions.get("window").width - padding * 2;
+    const boxHeight = boxWidth * 1.6;
+
+    const footerHeight = Dimensions.get("window").height * 0.8;
+    
+    function focusItem(item: OnboardingCardAttributes | null) {
+        if (item === null || item.index === 0) {
+            todayButtonOpacity.value = withTiming(0, { duration: 200 });
+            todayButtonTransformY.value = withTiming(-50, { duration: 200 });
+        } else {
+            todayButtonOpacity.value = withTiming(1, { duration: 1000 });
+            todayButtonTransformY.value = withTiming(0, { duration: 1000 });
+        }
+
+        if (item === null) {
+            setFocusedIndex(item);
+            outerBackgroundColor.value = withTiming(theme.background, { duration: 200 });
+            goButtonColor.value = withTiming('transparent', { duration: 200 });
+            goButtonOpacity.value = withTiming(0, { duration: 200 });
+        } else {
+            setFocusedIndex(item.index);
+            outerBackgroundColor.value = withTiming(item.colors.outerBackgroundColor, { duration: 200 });
+            goButtonColor.value = withTiming(item.colors.textColor, { duration: 200 });
+            goButtonOpacity.value = withTiming(1, { duration: 200 });
+        }
+    }
+
+    /**
+     * Scroll to card with index
+     */
+    async function scrollToItem(index: number) {
+        flatListRef.current?.scrollToIndex({ index, viewPosition: 0.5 });
+    }
 
     return (
-        <View style={{ flex: 1, backgroundColor: theme.background }}>
-            <ViewPager style={{ flex: 1 }} ref={pagerRef}>
-                <View key="1">
-                    <OnboardingPage
-                        prevButtonCallback={() => { navigation.navigate('start') }}
-                        nextButtonCallback={() => { handlePageChange(1) }}
-                    >
-                        <View style={{ flex: 1, margin: 50, ...Styles.centeringContainer }}>
-                        <Text style={{fontSize: 30, textAlign: 'center', marginBottom: 20}}>What should we call you?</Text>
-                        <KeyboardAvoidingView
-                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-                        style={{ flex: 0, width: '90%' }}>
-                        <TextInput placeholder={'Username'} onChangeText={setUsername} style={{ marginBottom: 10 }} />
-                        </KeyboardAvoidingView>
-                        </View>
-                    </OnboardingPage>
-                </View>
-                <View key="2">
-                    <OnboardingPage
-                        prevButtonCallback={() => { handlePageChange(0) }}
-                        nextButtonCallback={() => { handlePageChange(2) }}
-                    >
-                        <View style={{ flex: 1, margin: 10, ...Styles.centeringContainer }}>
-                            <Text style={{fontSize: 25, textAlign: 'center'}}>Your username is how you will be identified by friends...</Text>
-                            <Image source = {require('@/assets/friends.png')} style = {{marginTop: -20, width: 475, height: 475}}/>
-                        </View>
-                    </OnboardingPage>
-                </View>
-                <View key="3">
-                    <OnboardingPage
-                        prevButtonCallback={() => { handlePageChange(1) }}
-                        nextButtonCallback={() => { handlePageChangePoints(3) }}
-                    >
-                        <View style={{ flex: 1, margin: 10, ...Styles.centeringContainer }}>
-                            <Text style={{fontSize: 25, textAlign: 'center'}}>And how you'll be recognized for awards!</Text>
-                            <View style = {{padding: 40, flexDirection: 'row'}}>
-                            <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-start'}}>1</Text>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Image style = {{width: 100, height: 100, marginTop: -25, marginLeft: 0, marginRight: -30}} source = {require('@/assets/goldMedal.png')}/>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-end', marginLeft: -25}}>{username}</Text>
-                                </View>
-                            </View>
-                            <View style = {{padding: 40, flexDirection: 'row', marginTop: -60}}>
-                            <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-start', marginRight: 30}}>2</Text>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Image style = {{width: 100, height: 100, marginTop: -25, marginLeft: 0, marginRight: -30}} source = {require('@/assets/silverMedal.png')}/>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-end', marginLeft: -25}}>Bryan</Text>
-                                </View>
-                            </View>
-                            <View style = {{padding: 40, flexDirection: 'row', marginTop: -60}}>
-                            <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-start', marginRight: 30}}>3</Text>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Image style = {{width: 100, height: 100, marginTop: -25, marginLeft: 0, marginRight: -30}} source = {require('@/assets/bronzeMedal.png')}/>
-                                </View>
-                                <View style = {{flex: 1}}>
-                                <Text style ={{fontSize:25, marginTop: 10, justifyContent: 'flex-end', marginLeft: -25}}>Bob</Text>
-                                </View>
-                            </View>
-                            
-                            
-                        </View>
-                    </OnboardingPage>
-                </View>
-                
-                <View key="4">
-                    <OnboardingPage
-                    prevButtonCallback={() => { handlePageChangePointsReset(2) }}
-                    nextButtonCallback={() => { handlePageChangePointsReset(4) }}
-                    >
-                        <View style={{ flex: 1, margin: 10, ...Styles.centeringContainer }}>
-                            <Text style={{fontSize:25, textAlign: 'center'}}>Earn points by completing lessons and rise up the ranks!</Text>
-                            <AnimatedNumbers
-                        animateToNumber={points}
-                        animationDuration= {1600}
-                        fontStyle={{ color: theme.text, fontSize: 100, textAlign: 'center', fontFamily: 'Silkscreen_400Regular' }}
-                        easing={Easing.out(Easing.cubic)}
-                    />
-
-                        </View>
-                    </OnboardingPage>
-                </View>
-                <View key="5">
-                    <OnboardingPage
-                        buttonText="TAKE OFF!"
-                        prevButtonCallback={() => { handlePageChangePoints(3) }}
-                        nextButtonCallback={() => { navigation.navigate("signup") }}
-                    >
-                        <View style={{ flex: 1, margin: 10, ...Styles.centeringContainer }}>
-                            <Text style={{fontSize: 50, marginBottom: 0, textAlign: 'center'}}>Get ready to take flight,</Text>
-                            <Text style={{fontSize: 50, marginBottom: 30, textAlign: 'center'}}>{username}</Text>
-                        </View>
-                    </OnboardingPage>
-                </View>
-            </ViewPager >
-        </View >
+        <Animated.View style={{
+            flex: 1,
+            backgroundColor: outerBackgroundColor,
+            paddingHorizontal: padding,
+        }}>
+            <FlatList
+                ref={flatListRef}
+                renderItem={({ item, index }) => {
+                    return (
+                        <Pressable onPressIn={() => {
+                            item.ref?.onPressIn();
+                            scrollToItem(index);
+                        }} onPressOut={item.ref?.onPressOut}>
+                            <OnboardingCard
+                                ref={(ref: OnboardingCardRef) => { item.ref = ref }}
+                                focused={focusedIndex === item.index}
+                                colors={item.colors}
+                                style={{
+                                    height: boxHeight
+                                }}
+                            />
+                        </Pressable>
+                    )
+                }}
+                data={cards}
+                ListHeaderComponent={<OnboardingHeaderComponent height={headerHeight} />}
+                ListFooterComponent={<ListFooterComponent height={footerHeight} />}
+                getItemLayout={(_, index) => {
+                    return {
+                        length: boxHeight + padding,
+                        offset: (boxHeight + padding) * index + (paddingAboveHeader + headerHeight + padding),
+                        index
+                    }
+                }}
+                keyExtractor={(item) => item.index.toString()}
+                numColumns={1}
+                onViewableItemsChanged={({ viewableItems }) => {
+                    if (viewableItems.length === 0) {
+                        // focus header or footer or between items
+                        focusItem(null);
+                    } else {
+                        focusItem(viewableItems[0].item);
+                    }
+                }}
+                viewabilityConfig={{
+                    itemVisiblePercentThreshold: 75,  // how much of the item is visible
+                    waitForInteraction: false
+                }}
+                contentContainerStyle={{ gap: padding, paddingTop: paddingAboveHeader }}
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                decelerationRate='fast'
+                snapToOffsets={cards.map((_, i) =>
+                    (boxHeight + padding) * i
+                    + paddingAboveHeader + headerHeight + padding
+                    - Dimensions.get("window").height * 0.5 + boxHeight / 2
+                )}
+            />
+            
+        </Animated.View>
     );
 }
---
+++ src/components/2d/OnboardingHeaderComponent.tsx
@@ -0,0 +1,33 @@
+import { Styles } from "@/styles";
+import { useFirebase } from "@/utils/FirebaseProvider";
+import { useNavigation } from "@react-navigation/native";
+import { View } from "react-native";
+import Button from "./Button";
+import Plane from "./Plane";
+import Text from "./Text";
+import TextInput from "./TextInput";
+
+export default function OnboardingHeaderComponent({ height }: { height: number }) {
+    const firebase = useFirebase();
+    const navigation = useNavigation<any>();
+    return (
+        <View style={{
+            height,
+            justifyContent: 'space-between',
+        }}>
+            <View style={{
+                flex: 1,
+                ...Styles.centeringContainer,
+            }}>
+                <Text style={{ fontSize: 40, textAlign: 'center', marginBottom: 30 }}>What should we call you?</Text>
+                <TextInput
+                    placeholder={'Username'}
+                    style={{ fontSize: 20, textAlign: 'center', marginBottom: 30, height: 50, width: '40%' }}
+                />
+            </View>
+            <View style={{ ...Styles.centeringContainer }}>
+                <Plane onPress={() => { }} />
+            </View>
+        </View>
+    );
+}
