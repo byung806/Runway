7d5e9161b1e4a19443ade95cbb95e91006ae31e4
Bryan Yung <byung806@gmail.com>
2024-07-27 22:57:09 -0400

    Add friend adding UI

---
+++ src/components/2d/AddFriendModal.tsx
@@ -0,0 +1,44 @@
+import Text from './Text';
+import { Modal, View } from 'react-native';
+import TextInput from './TextInput';
+import Button from './Button';
+import { useState } from 'react';
+import { useFirebase } from '@/utils/FirebaseProvider';
+import { Styles } from '@/styles';
+
+export default function AddFriendModal({ visible, setVisible }: { visible: boolean, setVisible: (visible: boolean) => void }) {
+    const firebase = useFirebase();
+
+    const [username, setUsername] = useState('');
+    const [addingFriend, setAddingFriend] = useState(false);
+    const [message, setMessage] = useState('');
+
+    async function addFriend() {
+        setAddingFriend(true);
+        const { success } = await firebase.addFriend(username);
+        if (success) {
+            setMessage('Friend added!');
+        }
+        else {
+            setMessage('Failed to add friend!');
+        }
+        setAddingFriend(false);
+    }
+
+    // TODO: convert this into Friends screen and add current friends list
+    return (
+        <Modal visible={visible} animationType='slide'>
+            <View style={{
+                flex: 1,
+                gap: 10,
+                ...Styles.centeringContainer
+            }}>
+                <Text style={{ fontSize: 40, textAlign: 'center', marginBottom: 30 }}>Add Friend</Text>
+                <TextInput placeholder="Enter username" onChangeText={setUsername} style={{ width: '80%', height: 50 }} />
+                <Button title="Add" onPress={addFriend} style={{ width: '80%', height: 50 }} disabled={addingFriend} />
+                {message ? <Text style={{ fontSize: 15, textAlign: 'center', marginVertical: 5 }}>{message}</Text> : null}
+                <Button title="Cancel" backgroundColor='transparent' onPress={() => { setVisible(false) }} />
+            </View>
+        </Modal>
+    );
+}
--- src/components/2d/ListHeaderComponent.tsx
+++ src/components/2d/ListHeaderComponent.tsx
@@ -1,11 +1,12 @@
 import { Styles } from "@/styles";
 import { useFirebase } from "@/utils/FirebaseProvider";
 import { useNavigation } from "@react-navigation/native";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { View } from "react-native";
 import Button from "./Button";
 import Text from "./Text";
 import { ThemeContext } from "./ThemeProvider";
+import AddFriendModal from "./AddFriendModal";
 
 
 export default function ListHeaderComponent({ height, arrowDown }: { height: number, arrowDown: JSX.Element }) {
@@ -13,16 +14,14 @@ export default function ListHeaderComponent({ height, arrowDown }: { height: num
     const navigation = useNavigation<any>();
     const theme = useContext(ThemeContext);
 
+    const [addFriendModalVisible, setAddFriendModalVisible] = useState(false);
+
     async function checkUncompletedChallengeToday() {
         const uncompletedChallengeToday = await firebase.checkUncompletedChallengeToday();
         console.log('uncompletedChallengeToday', uncompletedChallengeToday);
         if (uncompletedChallengeToday) {
             navigation.navigate('content');
         }
-    }
-
-    async function addFriend(username: string) {
-        const { success } = await firebase.addFriend(username);
     }
 
     async function logOut() {
@@ -35,21 +34,30 @@ export default function ListHeaderComponent({ height, arrowDown }: { height: num
             justifyContent: 'space-between',
         }}>
             <View style={{
-                gap: 10,
+                justifyContent: 'space-between',
+                flexDirection: 'row',
             }}>
-                <Button title="Log Out" onPress={logOut} style={{ width: '80%' }} />
-                <Button title="Leaderboard" onPress={() => navigation.navigate('leaderboard')} style={{ width: '80%' }} />
+                <Button title="Add Friend" onPress={() => { setAddFriendModalVisible(true); }} />
+                <Button title="Log Out" onPress={logOut} backgroundColor='transparent' />
             </View>
+
             <View style={{
                 flex: 1,
                 ...Styles.centeringContainer,
             }}>
                 <Text style={{ fontSize: 40, ...Styles.lightShadow, color: theme.runwayTextColor }}>{firebase.userData?.username}</Text>
                 <Text style={{ fontSize: 100, ...Styles.lightShadow, color: theme.runwayTextColor }}>{firebase.userData?.points}</Text>
+                <Text style={{ fontSize: 30, ...Styles.lightShadow, color: theme.runwayTextColor }}>points</Text>
+                <Button title="Leaderboard" onPress={() => navigation.navigate('leaderboard')} style={{ width: '80%', marginTop: 20 }} />
             </View>
             <View style={{ ...Styles.centeringContainer }}>
                 {arrowDown}
             </View>
+
+            <AddFriendModal
+                visible={addFriendModalVisible}
+                setVisible={setAddFriendModalVisible}
+            />
         </View>
     );
 }
