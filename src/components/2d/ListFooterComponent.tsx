d86399e4fa7d5228bc8121074bfdd4e5aaa081a7
Bryan Yung <byung806@gmail.com>
2024-07-31 01:12:17 -0400

    Fix footer colors

--- src/components/2d/ListFooterComponent.tsx
+++ src/components/2d/ListFooterComponent.tsx
@@ -1,16 +1,19 @@
 import { Styles } from "@/styles";
+import { useContext } from "react";
 import { View } from "react-native";
 import Text from "./Text";
+import { ThemeContext } from "./ThemeProvider";
 
 export default function ListFooterComponent({ height }: { height: number }) {
+    const theme = useContext(ThemeContext);
     return (
         <View style={{
             height,
             gap: 20,
             ...Styles.centeringContainer,
         }}>
-            <Text style={{ fontSize: 40 }}>Houston, we have a problem...</Text>
-            <Text style={{ fontSize: 20, textAlign: 'center', marginBottom: 60 }}>Please check back later for content!</Text>
+            <Text style={{ fontSize: 40, color: theme.white }}>Houston, we have a problem...</Text>
+            <Text style={{ fontSize: 20, textAlign: 'center', marginBottom: 60, color: theme.white }}>Please check back later for content!</Text>
         </View>
     );
 }
