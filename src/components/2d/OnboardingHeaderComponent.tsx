129f04791ac5987b6d45d604bdd20101c197b46f
Bryan Yung <byung806@gmail.com>
2024-09-20 00:10:48 -0400

    Check username availability during signup & refactor

--- src/components/2d/OnboardingHeaderComponent.tsx
+++ src/components/2d/OnboardingHeaderComponent.tsx
@@ -1,40 +1,64 @@
 import { ThemeContext, useFirebase } from "@/providers";
 import { Styles } from "@/styles";
+import { matcher } from '@/utils/ProfanityChecker';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import { useNavigation } from "@react-navigation/native";
 import { Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
 import { KeyboardAvoidingView, LayoutAnimation, Platform, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { BackArrow, ScrollArrow } from "./Arrow";
 import Text from "./Text";
 import TextInput from "./TextInput";
-import { BackArrow } from "./Arrow";
-import { useIsFocused, useNavigation } from "@react-navigation/native";
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 
-
-export default function OnboardingHeaderComponent({ height, username, setUsername, arrowDown }: { height: number, username: string, setUsername: Dispatch<SetStateAction<string>>, arrowDown: JSX.Element }) {
+export default function OnboardingHeaderComponent({ focused, scrollable, setScrollable, scroll, height, username, setUsername }: { focused: boolean, scrollable: boolean, setScrollable: Function, scroll: () => void, height: number, username: string, setUsername: Dispatch<SetStateAction<string>> }) {
     const theme = useContext(ThemeContext);
     const navigation = useNavigation<any>();
     const firebase = useFirebase();
-    const focused = useIsFocused();
 
     const [loading, setLoading] = useState<boolean>(false);
     const [usernameAvailable, setUsernameAvailable] = useState<boolean>(false);
 
+    /**
+     * Check if a username is available
+     */
     async function checkUsername(username: string) {
-        console.log('checking username', username);
+        // console.log('checking username', username);
+        if (username.length < 3) {
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+            setUsernameAvailable(false);
+            setLoading(false);
+            setScrollable(false);
+            return;
+        }
+        if (username.length > 15) {
+            LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+            setUsernameAvailable(false);
+            setLoading(false);
+            setScrollable(false);
+            return;
+        }
+        if (matcher.hasMatch(username)) {
+            setUsernameAvailable(false);
+            setLoading(false);
+            setScrollable(false);
+            return
+        }
+
+        setUsernameAvailable(false);
         setLoading(true);
+
         const good = await firebase.isUsernameAvailable(username);
-        console.log(username, 'available?', good);
+        // console.log(username, 'available?', good);
+
         setLoading(false);
         setUsernameAvailable(good);
+        setScrollable(good);
     }
 
-    useEffect(() => {
-        if (focused) {
-            LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
-        }
-    }, [focused]);
-
+    /**
+     * Animate the layout when the username changes (if check/cross or error messages appear/disappear)
+     */
     useEffect(() => {
         LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
     }, [username]);
@@ -74,10 +98,7 @@ export default function OnboardingHeaderComponent({ height, username, setUsernam
                         <TextInput
                             value={username}
                             onChangeText={(text: string) => {
-                                if (text.length >= 3 && text.length <= 15) {
-                                    setUsernameAvailable(false);
-                                    checkUsername(text);
-                                }
+                                checkUsername(text);
                                 setUsername(text);
                             }}
                             maxLength={16}
@@ -87,9 +108,12 @@ export default function OnboardingHeaderComponent({ height, username, setUsernam
                         {username.length >= 3 && username.length <= 15 && usernameAvailable ?
                             <FontAwesome5 name="check" size={24} color={theme.questionCorrectColor} />
                             : null}
+                        {username.length >= 3 && username.length <= 15 && !usernameAvailable && !loading ?
+                            <FontAwesome5 name="times" size={24} color={theme.questionIncorrectColor} />
+                            : null}
                     </View>
-                    {!usernameAvailable && !loading && username.length >= 3 && username.length <= 15 &&
-                        <Text style={{ fontSize: 15, textAlign: 'center', marginVertical: 5, color: theme.runwayTextColor }}>This username is taken!</Text>
+                    {!usernameAvailable && !loading && username.length >= 3 && username.length <= 15 && !loading &&
+                        <Text style={{ fontSize: 15, textAlign: 'center', marginVertical: 5, color: theme.questionIncorrectColor }}>Please choose a different username!</Text>
                     }
                     {username.length > 0 && username.length < 3 &&
                         <Text style={{ fontSize: 15, textAlign: 'center', marginVertical: 5, color: theme.runwayTextColor }}>At least 3 characters please!</Text>
@@ -97,12 +121,14 @@ export default function OnboardingHeaderComponent({ height, username, setUsernam
                     <Text style={{ fontSize: 18, textAlign: 'center', color: theme.runwaySubTextColor }}>This will be public to everyone!</Text>
                 </View>
             </KeyboardAvoidingView>
-            {arrowDown && usernameAvailable ?
-                <SafeAreaView style={{ position: 'absolute', width: '100%', bottom: 0, ...Styles.centeringContainer, padding: 20, gap: 10 }} edges={['bottom']}>
-                    {arrowDown}
-                </SafeAreaView>
-                : null
-            }
+            
+            <SafeAreaView style={{ position: 'absolute', width: '100%', bottom: 0, ...Styles.centeringContainer, padding: 20, gap: 10 }} edges={['bottom']}>
+                <ScrollArrow
+                    type='down'
+                    visible={focused && usernameAvailable}
+                    onPress={scroll}
+                />
+            </SafeAreaView>
         </View>
     );
-}
+};
