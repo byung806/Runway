41a64c270f2245f3093e812fc58a5d53ece57d0b
Bryan Yung <byung806@gmail.com>
2025-01-28 23:22:36 -0500

    Implement click scale animation with react-spring
    Use max nodes instead of dynamically creating animated values. Tweak translate logic. Fix node group translation & scale.

--- src/components/2d/ForceGraphD3New.tsx
+++ src/components/2d/ForceGraphD3New.tsx
@@ -35,28 +35,32 @@
 // TODO: fade in out of screen?
 // TODO: add shadow/glow to nodes
 // TODO: add "special" nodes for leaderboard and profile and credits and payment
+// IMPORTANT: WHEN REMOVING NODES MAKE SURE TO CANCELANIMATION ON THE SCALE i think i have to do this? or maybe it'll be garbage collected idk
 
-import React, { useContext, useEffect, useMemo, useRef, useState } from 'react';
-import { View, Dimensions } from 'react-native';
-import Svg, { Circle, G, Line, Text } from 'react-native-svg';
+import { useRunwayTheme } from '@/providers';
 import * as d3 from 'd3';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { Dimensions, View } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import { ThemeContext } from '@/providers';
+import Svg, { Circle, G, Line, Text } from 'react-native-svg';
+import * as Haptics from 'expo-haptics';
 
-import Animated, { runOnUI, cancelAnimation, useSharedValue, useAnimatedStyle, withDecay } from 'react-native-reanimated';
+import Animated, { cancelAnimation, useAnimatedStyle, useSharedValue, withDecay } from 'react-native-reanimated';
+import { useSprings, animated as animatedSpring } from '@react-spring/native';
 
-const AnimatedG = Animated.createAnimatedComponent(G);
+const AnimatedGForPanning = Animated.createAnimatedComponent(G);
+const AnimatedGForNodes = animatedSpring(G);
 
 interface Node {
     x: number;
     y: number;
     vx?: number;
     vy?: number;
-    fx?: number | null;  // forced x
-    fy?: number | null;  // forced y
+    fx?: number | null;
+    fy?: number | null;
     radius: number;
     id: number;
-    degree: number;  // number of edges
+    degree: number; 
 }
 
 interface Link {
@@ -65,7 +69,7 @@ interface Link {
 }
 
 export default function ForceGraphD3New() {
-    const theme = useContext(ThemeContext);
+    const theme = useRunwayTheme();
 
     const [nodes, setNodes] = useState<Node[]>([]);
     const [links, setLinks] = useState<Link[]>([]);
@@ -79,28 +83,33 @@ export default function ForceGraphD3New() {
     const prevTranslateY = useSharedValue(0);
     const translateX = useSharedValue(0);
     const translateY = useSharedValue(0);
-    const scale = useSharedValue(1);
 
     const animatedStyle = useAnimatedStyle(() => {
         return {
             transform: [
                 { translateX: translateX.value },
                 { translateY: translateY.value },
-                { scale: scale.value },
             ],
         };
     });
 
+    // Set up react-spring for node scales. Assuming a maximum of 50 nodes for safety.
+    const MAX_NODES = 50;
+    const [springs, api] = useSprings(MAX_NODES, index => ({ scale: 1 }));
+
     // ------------------ Initialization ------------------
     useEffect(() => {
         // initialize nodes
-        const initNodes = d3.range(4).map((val): Node => ({
-            radius: Math.floor(Math.random() * 4) + 16,
+        const initNodes = d3.range(20).map((val): Node => ({
+            // radius: Math.floor(Math.random() * 4) + 30,
+            radius: 30,
             id: val,
             degree: 0,
             x: Math.random() * width,
-            y: Math.random() * height
+            y: Math.random() * height,
         }));
+
+        console.log('nodes successfully initialized');
 
         // node that follows touch (repels other nodes to create a cool effect when panning)
         initNodes[0].radius = 0;
@@ -116,29 +125,26 @@ export default function ForceGraphD3New() {
         }
 
         // initialize force https://d3js.org/d3-force/
+        const touchRepelStrength = -1200;
         const targetEdgeLength = minScreenDim / 6;
         const targetNodeSeparation = minScreenDim;
         const sim = d3.forceSimulation<Node>(initNodes)
             // repel when too close
             .force('repel', d3.forceManyBody<Node>()
-                .strength((d, i) => {
-                    if (i === 0) {
-                        return 0;
-                    }
-                    return -800;
-                })
+                .strength((d, i) => (i === 0 ? touchRepelStrength : -3000))
                 .distanceMax(targetNodeSeparation))
             // attract when too far
             .force('attract', d3.forceManyBody<Node>()
-                .strength((d, i) => 200)
+                .strength(() => 200)
                 .distanceMin(targetNodeSeparation))
             // attract linked nodes more
             .force('link', d3.forceLink<Node, Link>(initLinks)
                 .distance(targetEdgeLength)
                 .strength(2))
             // center the graph
-            // TODO: this just straight up changes positions so it causes a problem where when u press the invisible node the center of the graph changes and so it teleports
-            .force('center', d3.forceCenter(width / 2, height / 2).strength(1.1))
+            // you can't use forceCenter bc it force changes positions so it causes a problem where when u press the invisible node the center of the graph changes and so it teleports
+            .force('centerX', d3.forceX(width / 2))
+            .force('centerY', d3.forceY(height / 2))
             // prevent nodes from overlapping
             .force('collision', d3.forceCollide<Node>().radius(d => d.radius + 5))
             .on('tick', () => {
@@ -147,47 +153,36 @@ export default function ForceGraphD3New() {
             });
 
         forceRef.current = sim;
-
         setNodes(initNodes);
         setLinks(initLinks);
-
     }, [width, height]);
 
     /*
     * When panning, update the transform of the svg group
     */
     const panGesture = Gesture.Pan()
-        // if user touches, cancel inertia
         .onTouchesDown(() => {
-            // save the previous translation
             cancelAnimation(translateX);
-            prevTranslateX.value = translateX.value;
             cancelAnimation(translateY);
+            prevTranslateX.value = translateX.value;
             prevTranslateY.value = translateY.value;
-
-            forceRef.current?.alpha(0.1).restart();
         })
-        // panning begins
         .onStart(({ x, y }) => {
-            // subtract to get touch coords relative to the graph
-            nodes[0].fx = x - translateX.value;
-            nodes[0].fy = y - translateY.value;
-
-            // when alpha is 0, the simulation stops so we need to make it redo the physics a little bit
+            if (nodes[0]) {
+                nodes[0].fx = x - translateX.value;
+                nodes[0].fy = y - translateY.value;
+            }
             forceRef.current?.alpha(0.1).restart();
         })
-        // during panning
         .onUpdate(({ translationX, translationY }) => {
             translateX.value = prevTranslateX.value + translationX;
             translateY.value = prevTranslateY.value + translationY;
         })
-        // panning ends
         .onEnd(({ velocityX, velocityY }) => {
-            // add inertia
-            translateX.value = withDecay({ velocity: velocityX, deceleration: 0.9995 });
-            translateY.value = withDecay({ velocity: velocityY, deceleration: 0.9995 });
+            translateX.value = withDecay({ velocity: velocityX, deceleration: 0.999 });
+            translateY.value = withDecay({ velocity: velocityY, deceleration: 0.999 });
 
-            // TODO: snap to node
+            // TODO: snap to node?
             // find closest node to center of screen (node coordinates have to be shifted by the transform amount)
             // const closestNode = nodes.reduce((prev, curr) => {
             //     const prevDist = Math.hypot(
@@ -209,19 +204,30 @@ export default function ForceGraphD3New() {
             // fix node so it doesn't move
             // closestNode.fx = closestNode.x;
             // closestNode.fy = closestNode.y;
+        })
+        .runOnJS(true);
 
-            // console.log('velocity:', velocityX, velocityY);
-        }).runOnJS(true);
+    function nodePressedIn(node: Node) {
+        const currentScale = springs[node.id]?.scale.get();
+        api.start(i => (i === node.id ? { scale: currentScale > 1 ? 1 : 1.5 } : { scale: 1 }));
+        
+        console.log('node pressed in:', node.id);
+    }
 
     function nodePressed(node: Node) {
-        console.log('node pressed:', node.id);
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    }
+
+    function nodePressedOut(node: Node) {
+        // api.start(i => (i === node.id ? { scale: 1 } : {}));
+        console.log('node pressed out:', node.id);
     }
 
     return (
         <GestureDetector gesture={panGesture}>
             <View style={{ flex: 1 }}>
                 <Svg style={{ flex: 1 }}>
-                    <AnimatedG animatedProps={animatedStyle}>
+                    <AnimatedGForPanning animatedProps={animatedStyle}>
                         {links.map((link, i) => (
                             <Line
                                 key={`link-${i}`}
@@ -235,19 +241,27 @@ export default function ForceGraphD3New() {
                             />
                         ))}
                         {nodes.slice(1).map((node) => (
-                            <G key={`node-${node.id}`}>
+                            <AnimatedGForNodes
+                                key={`node-${node.id}`}
+                                x={node.x}
+                                y={node.y}
+                                // @ts-expect-error
+                                style={{
+                                    // transform adds to the parent transform
+                                    transform: [
+                                        { scale: springs[node.id]?.scale ?? 1 }
+                                    ]
+                                }}
+                            >
                                 <Circle
-                                    cx={node.x}
-                                    cy={node.y}
                                     r={node.radius}
                                     fill={d3.schemeSet3[node.id % 12]}
+                                    onPressIn={() => nodePressedIn(node)}
                                     onPress={() => nodePressed(node)}
+                                    onPressOut={() => nodePressedOut(node)}
                                     opacity={1}
                                 />
-                                {/* debug text to show each node's id */}
                                 <Text
-                                    x={node.x}
-                                    y={node.y}
                                     fontSize={node.radius}
                                     fill="#000"
                                     textAnchor="middle"
@@ -255,19 +269,11 @@ export default function ForceGraphD3New() {
                                 >
                                     {node.id}
                                 </Text>
-                            </G>
+                            </AnimatedGForNodes>
                         ))}
-                        {/* reference for the center */}
-                        <Circle
-                            cx={width / 2}
-                            cy={height / 2}
-                            r={30}
-                            fill={d3.schemeSet3[0]}
-                            opacity={0.8}
-                        />
-                    </AnimatedG>
+                    </AnimatedGForPanning>
                 </Svg>
             </View>
         </GestureDetector>
     );
-};
+}
